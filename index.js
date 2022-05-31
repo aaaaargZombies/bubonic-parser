8a712f2f2b5b040968b599d1c34d57bad5cd3add
aaaaargZombies <ahberate@riseup.net>
2022-05-31 12:23:09 +0100

    feat: move comments to relevant part of code

--- index.js
+++ index.js
@@ -13,18 +13,14 @@ const isTag = (s) =>
   (s.length === 1 && s === "<") || (/^<\w|^<\//.test(s) && !/<[0-9]/.test(s));
 
 const parse = (string, tag = "b") => {
-  // iterate over string pushing letters onto an expression.
-  // check if expression is part of HTML tag, don't add extra markup into this
-  // if not compare end of expression and current point in string to detect word boundaries
-  // if no boundary detected continue adding to expression
-  // if we hit a boundary add the new expression to result via a test for letters (adding tags if its a word).
-  // clear the expression and continue.
-  // combine the result and remaining expression at end of while loop and return
   let pointer = 1;
   let result = "";
   let expression = string[0];
+  // iterate over string pushing letters onto an expression.
   while (pointer < string.length) {
+    // check if expression is part of HTML tag, don't add extra markup into this
     if (isTag(expression)) {
+      // if tag is closed add the expression to the result, clear the expression, move the pointer.
       if (/>$/.test(expression)) {
         result = result + expression;
         expression = string[pointer];
@@ -34,23 +30,28 @@ const parse = (string, tag = "b") => {
         pointer++;
       }
     } else {
+      // if not a tag compare end of expression and current point in string to detect word boundaries
       if (
         notAWordBoundary(expression[expression.length - 1], string[pointer]) &&
+        // if no boundary detected continue adding to expression
         string[pointer] !== "<"
       ) {
         expression = expression + string[pointer];
         pointer++;
       } else {
+        // if we hit a boundary add the acumulated expression to result via a test for letters (adding tags if its a word).
         result =
           result +
           (wordRegEx.test(expression[0])
             ? addTag(expression, tag)
             : expression);
+        // clear the expression and continue.
         expression = string[pointer];
         pointer++;
       }
     }
   }
+  // combine the result and remaining expression at end of while loop and return
   return (
     result + (wordRegEx.test(expression[0]) ? addTag(expression) : expression)
   );
